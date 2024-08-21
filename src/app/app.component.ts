e5fa9432fb6a50f43138ca5535ac4df8d9574833
James Patterson <jamespatterson.feswe@yahoo.com>
2024-08-21 18:52:41 -0400

    fix: header comp, use in app comp, add tests, create footer and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
