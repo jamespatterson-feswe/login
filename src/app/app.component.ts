0fee8ad649a789c0fa316745fc7d11078ba25219
James Patterson <jamespatterson.feswe@yahoo.com>
2024-08-21 18:57:12 -0400

    fix: wire in the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
+    FooterComponent,
     RouterOutlet
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
